88357bd3329aeccb58519e07d683ea4e4a7d15d9
THILAKnaik@110 <thilaknaiktharipadpu@gmail.com>
2021-03-25 22:04:29 +0530

    bug fix

--- helpTexts.py
+++ helpTexts.py
@@ -12,7 +12,7 @@ After that follow instructions
 To get perticular subject date and time use /qpcode command
 
 1️⃣Send /qpcode with your subject QPCode
-    EX - /qpcode 12345
+    EX - /qpcode 1234
     type /qpcode give a single space then type QPCode and send
 2️⃣This will reply you Date and Time of that Subject
 ✅Done
